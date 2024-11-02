970bd36a260f2e31a1305b6e24b4d345e07b18ad
Ngọc Nghĩa <ngocnghia.workspace@gmail.com>
2024-11-02 08:34:54 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 window.addEventListener("load", function () {
     function TimerDate() {
         // this đại diện cho TimerDate
